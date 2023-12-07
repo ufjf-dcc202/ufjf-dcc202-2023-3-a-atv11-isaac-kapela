8f1a88a3f58c9279d42998324678a186e29e0615
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-06 23:40:18 -0300

    add lista.js

--- js.js
+++
@@ -1,9 +0,0 @@
-
-
-
-//adiciona os 3 primeiros itens
-atualizarListaOrdenada();
-
-//adiciona eventos do botao para adicionar e remover
-btnAdicionar.addEventListener('click',adicionaItemDaEntrada)
-btnLimpar.addEventListener('click',limparListaOrdenada)
